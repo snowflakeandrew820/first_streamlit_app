98444f52aad8034a0cd695322c4eac89d9e05227
snowflakeandrew820 <156622520+snowflakeandrew820@users.noreply.github.com>
2024-01-18 00:11:47 -0600

    Update streamlit_app.py
    delete the raw data display from Fruity

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  #just writes the data to the screen
+# remove the raw data
+#streamlit.text(fruityvice_response.json())  #just writes the data to the screen
 
 # Normalize the json output 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
