f225f2af844d5c4d12b04ca81825b128638b510e
snowflakeandrew820 <156622520+snowflakeandrew820@users.noreply.github.com>
2024-01-19 01:00:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 # query some real data
-my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruite load list contains:")
 streamlit.text(my_data_row)
